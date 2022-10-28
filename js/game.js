c95e3450c254cec861072a8106ffb67c0c62abeb
Juanma Stella <0x86jm14sMb94@gmail.com>
2022-10-28 19:58:11 +0200

    movement is succesful on both warriors.

--- js/game.js
+++ js/game.js
@@ -27,16 +27,34 @@ class Warrior {
     }
 
     update() {
+        let warriorGravity = gravity
         this.create()
+
+        if(this.position.y < 0){
+            this.position.y = 0
+            warriorGravity = 0.6
+        }
+
+        if(this.position.y < 90){
+            warriorGravity = 0.2
+        }
+
+        if(this.position.y < canvas.height/2 + 200){
+            warriorGravity = 0.2
+        }
         
         this.position.x += this.velocity.x
         this.position.y += this.velocity.y
 
+        
+
         if((this.position.y + this.height + this.velocity.y) >= canvas.height) {
             this.velocity.y = 0;
         }  else {
-            this.velocity.y += gravity
+            this.velocity.y += warriorGravity
         }
+
+        keys.w.pressed = false
     }
 
     attack() {
@@ -97,6 +115,18 @@ const keys = {
     },
     d: {
         pressed: false
+    },
+    ArrowUp: {
+        pressed: false
+    },
+    ArrowDown: {
+        pressed: false
+    },
+    ArrowLeft: {
+        pressed: false
+    },
+    ArrowRight: {
+        pressed: false
     }
 }
 
@@ -110,7 +140,15 @@ const animate = () => {
     warrior_2.update()
     
     warrior_1.velocity.x = 0
+    warrior_2.velocity.x = 0
 
+    
+    if ((keys.a.pressed && lastkey === "ArrowLeft") || keys.ArrowLeft.pressed) {
+        warrior_2.velocity.x = -9
+    } else if ((keys.d.pressed && lastkey === "ArrowRight") || keys.ArrowRight.pressed) {
+        warrior_2.velocity.x = 9
+    }
+    
     if ((keys.a.pressed && lastkey === "a") || keys.a.pressed) {
         warrior_1.velocity.x = -9
     } else if ((keys.d.pressed && lastkey === "d") || keys.d.pressed) {
@@ -118,16 +156,26 @@ const animate = () => {
     }
 }
 
-animate()
+// animate()
 
 window.addEventListener("keydown", (e) => {
     switch (e.key) {
         case "w":
-            warrior_1.velocity.y = -10
+                keys.w.pressed = true
+                if(!e.repeat){
+
+                    if (warrior_1.position.y < 50){
+                        warrior_1.velocity.y = -0.1
+                        
+                    } else if (warrior_1.position.y < canvas.height/2 + 200){
+                        warrior_1.velocity.y = -6
+                        
+                    } else warrior_1.velocity.y = -10
+                }
         break;
 
         case "s":
-        
+            warrior_1.velocity.y = 1
         break;
 
         case "a":
@@ -147,9 +195,47 @@ window.addEventListener("keydown", (e) => {
     console.log(e)
 })
 
+window.addEventListener("keydown", (e) => {
+    switch (e.key) {
+        case "ArrowUp":
+                keys.ArrowUp.pressed = true
+                if(!e.repeat){
+
+                    if (warrior_2.position.y < 50){
+                        warrior_2.velocity.y = -0.1
+                        
+                    } else if (warrior_2.position.y < canvas.height/2 + 200){
+                        warrior_2.velocity.y = -6
+                        
+                    } else warrior_2.velocity.y = -10
+                }
+        break;
+
+        case "ArrowDown":
+            warrior_2.velocity.y = 1
+        break;
+
+        case "ArrowLeft":
+            keys.ArrowLeft.pressed = true
+        
+        break;
+
+        case "ArrowRight":
+            keys.ArrowRight.pressed = true
+
+        
+        break;
+    
+        default:
+            break;
+    }
+    console.log(e)
+})
+
 window.addEventListener("keyup", (e) => {
     switch (e.key) {
         case "w":
+            keys.w.pressed = false
             
         break;
 
@@ -166,6 +252,36 @@ window.addEventListener("keyup", (e) => {
         case "d":
             keys.d.pressed = false
             lastkey = "a"
+
+        
+        break;
+    
+        default:
+            break;
+    }
+    console.log(e)
+})
+
+window.addEventListener("keyup", (e) => {
+    switch (e.key) {
+        case "ArrowUp":
+            keys.ArrowUp.pressed = false
+            
+        break;
+
+        case "ArrowDown":
+        
+        break;
+
+        case "ArrowLeft":
+            keys.ArrowLeft.pressed = false
+            lastkey = "ArrowRight"
+
+        break;
+
+        case "ArrowRight":
+            keys.ArrowRight.pressed = false
+            lastkey = "ArrowLeft"
 
         
         break;
